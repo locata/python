4250b7d052d5b635ec4d7ba233529a6e2137c91b
lc <lc@localhost.localdomain>
2016-06-03 20:15:36 -0400

    added python tutorial files

---
+++ pythonHacking02/crack.py
@@ -0,0 +1,39 @@
+import crypt
+import optparse
+
+def testPass(cryptPass, dname):
+	salt = cryptPass[0:2]
+	dictFile = open(dname, 'r')
+	for word in dictFile.readlines():
+		word = word.strip('\n\r')
+		cryptWord = crypt.crypt(word,salt)
+		if (cryptWord == cryptPass):
+			print "[+] Found Password: "+word+ "\n"
+			return
+	print "[-] Password not found. \n"
+	return
+
+def Main():
+	parser = optparse.OptionParser("usage %prog "+"-f <passwordFiles> -d <dictionary>")
+	parser.add_option('-f', dest='pname', type='string', help='specify password file')
+	parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+	(options, args) = parser.parse_args()
+	if (options.pname == None) | (options.dname == None):
+		print parser.usage
+		exit(0);
+	else:
+		pname = options.pname
+		dname = options.dname
+	
+	passFile = open(pname, 'r')
+	for line in passFile.readlines():
+		if ":" in line:
+			user = line.split(':')[0]
+			cryptPass = line.split(':')[1].strip(' ')
+			print "[+] cracking password for: "+user
+			testPass(cryptPass, dname)
+
+if __name__ == '__main__':
+	Main()
+
+
